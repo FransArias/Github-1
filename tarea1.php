904d35f45a92b1a69d3021b0f27d99c0d46cd26e
DESKTOP-QQ72R64\Frans <fransmiba@hotmail.com>
2017-02-04 01:24:24 -0500

    Proyecto-1

---
+++ tarea1.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>CUP 2017</title>
+	<link rel="stylesheet" type="text/css" href="clasejs13.css">
+</head>
+<body>
+
+<header class="header">Mi Header</header>
+<nav >Mi Nav</nav>
+<section>
+Mi Section
+	<article>Articulo 1</article>
+	<br>
+	<article>Articulo 2</article>
+	<br>
+	<article>Articulo 3</article>
+	<br>
+	<article>Articulo 4</article>
+	<br>
+	<article>Articulo 5</article>
+	<br>
+	<article>Articulo 6</article>
+
+</section>
+<footer>Mi Footer</footer>
+
+
+
+</body>
+</html>
